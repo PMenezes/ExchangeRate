f1bbc13859910324c192cc468f16f1d7a209477b
Pedro <pedro.brt.menezes@gmail.com>
2025-01-19 17:21:17 +0000

    Configure openapi for documentation with swagger and configure http security

---
+++ src/main/java/com/exchangerates/OpenApiConfig.java
@@ -0,0 +1,29 @@
+package com.exchangerates;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("Exchange Rates API")
+                        .version("1.0.0")
+                        .description("API to fetch and use exchange rates for conversion calculations."));
+    }
+}
+
